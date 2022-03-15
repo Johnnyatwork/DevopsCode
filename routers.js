d3ec85e416087bf08a00a7f2a2c057ff267d785b
John Daniel <Johnnyatwork@gmail.com>
2022-03-15 20:19:31 +0800

    First Commit

---
+++ routers.js
@@ -0,0 +1,13 @@
+// To import express library
+const express = require("express");
+
+// define a router object
+let router = express.Router();
+
+// define a GET API with path "/home"
+router.get("/", (request, response) => {
+    response.send("Welcome to NUS Money Backend!");
+  });
+
+
+module.exports = { router };
