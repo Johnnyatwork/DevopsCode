4efd78a993c913e7cc2da3e4ea2b38f7558d96f9
John Daniel <Johnnyatwork@gmail.com>
2022-03-15 20:25:52 +0800

    Second Commit

--- routers.js
+++ routers.js
@@ -4,10 +4,14 @@ const express = require("express");
 // define a router object
 let router = express.Router();
 
-// define a GET API with path "/home"
+// define a GET API with path "/"
 router.get("/", (request, response) => {
     response.send("Welcome to NUS Money Backend!");
   });
 
+  // define a GET API with path "/home"
+router.get("/home", (request, response) => {
+    response.send("Welcome to NUS Money Backend!");
+  });
 
 module.exports = { router };
